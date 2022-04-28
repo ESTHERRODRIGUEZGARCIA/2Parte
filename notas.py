eb7d836b9cd1b87b21832581e032f7d23146207a
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-28 09:32:00 +0200

    guardar

--- notas.py
+++ notas.py
@@ -13,7 +13,8 @@ reader = csv.reader(f)
 for row in reader:
     print (row)
 
-observaciones = pd.DataFrame({'NOTAS':np.array(['Pokemon.csv'])})
+observaciones = pd.read_csv("Pokemon.csv")
+
 print("— CANTIDAD DE OBSERVACIONES --")
 n = reader.count()
 print("Cantidad de observaciones = " + str(n))
@@ -29,49 +30,43 @@ valoresOrdenados = valoresOrdenados.reset_index(drop=True)
 print("Valor máximo: " + str(valoresOrdenados[len(valoresOrdenados)-1]))
 
 
-# funcion que cree una columna con el resultado de la media
-def calcular_media(dataset, col1, col2):
-    media = dataset[col2].sum()/dataset[col1].sum()
-    return media
+print ("\n-- MEDIA --")
+def calculoMediaAritmetica(caracteristica):
+   n = caracteristica.count()
+   sumaValorObservaciones = 0
+   mediaAritmetica = 0
 
-# funcion que me dev
-# dos es la multiplicación xi*ni
-class Media:
-    #Constructor
-    def __init__(self, dataset, col1, col2):
-        self.dataset = dataset
-        self.col1 = col1
-        self.col2 = col2
+   if n>0 :
+       for valorObservacion in caracteristica:
+           sumaValoresObservaciones = sumaValoresObservaciones + valorObservacion
+       mediaAritmetica = sumaValorObservaciones / n
 
-    def calcular_media(self):
+   return mediaAritmetica
 
-        def calcular_dos():
-            dos = []
-            for i in range (len(self.dataset)):
-                resultado = self.dataset[self.col1][i]*self.dataset[self.col2][i]
-                dos.append(resultado)
-            return dos
+def calculoMediana(caracteristica):
+   mediana = 0
+   caracteristica = caracteristica.sort_values()
+   caracteristica = caracteristica.reset_index(drop=True)
+   n = self.caracteristica.count()
+   par = False
+   if (n % 2 == 0):
+       print("La cantidad de observaciones es par.")
+       par = True
 
-        self.dataset["XiNi"] = calcular_dos()
-        suma_dos = self.dataset["DOS"].sum()
-        suma_ni = self.dataset[self.col2].sum()
+   if par:
+       rango = (n / 2)
+       rangoPython = rango-1
+       valor1 = caracteristica[rangoPython]
+       valor2 = caracteristica[rangoPython+1]
+       mediana = valor1 +((valor2-valor1)/2)
+   else:
+       rango = ((n + 1) / 2)
+       rangoPython = rango - 1
+       mediana = caracteristica [rangoPython]
+   return mediana
 
-        return suma_dos/suma_ni
 
 
-def calcular_std(dataset, col1, col2):
-
-    def columna_std():
-        columna = []
-        media = calcular_media(dataset, col1, col2)
-        for i in range(len(dataset)):
-            resultado = dataset[col2][i] * (dataset[col1][i] - media)*(dataset[col1][i] - media)
-            columna.append(resultado)
-        return columna
-
-    dataset["Ni*((Xi-media)^2)"] = columna_std()
-    suma_columna = dataset["Ni*((Xi-media)^2)"].sum()
-    suma_ni = dataset[col2].sum()
-    varianza = suma_columna/suma_ni
-
-    return sqrt(varianza)
+def calculoModa(caracteristica):
+   moda = Counter(caracteristica)
+   return moda
